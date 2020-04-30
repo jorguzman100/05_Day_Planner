8688185dda71c18f501e478a5a5a0027d7af0a11
Jorge Guzman <jorguzman100@gmail.com>
2020-04-29 20:00:56 -0500

    Display and Save functions defined with localStorage

---
+++ script.js
@@ -0,0 +1,131 @@
+$(document).ready(function () {
+  /* ---------- Global variables ---------- */
+  var activitiesArray = [
+    {
+      time: "8 am",
+      activity: "Activity at 9 am",
+      button: "Button1",
+    },
+    {
+      time: "9 am",
+      activity: "Activity at 10 am",
+      button: "Button2",
+    },
+    {
+      time: "10 am",
+      activity: "Activity at 11 am",
+      button: "Button3",
+    },
+    {
+      time: "11 am",
+      activity: "Activity at 12 pm",
+      button: "Button4",
+    },
+    {
+      time: "12 pm",
+      activity: "",
+      button: "Button",
+    },
+    {
+      time: "1 pm",
+      activity: "",
+      button: "Button",
+    },
+    {
+      time: "2 pm",
+      activity: "",
+      button: "Button",
+    },
+    {
+      time: "3 pm",
+      activity: "",
+      button: "Button",
+    },
+    {
+      time: "4 pm",
+      activity: "",
+      button: "Button",
+    },
+    {
+      time: "5 pm",
+      activity: "",
+      button: "Button",
+    },
+    {
+      time: "6 pm",
+      activity: "",
+      button: "Button",
+    },
+    {
+      time: "7 pm",
+      activity: "",
+      button: "Button",
+    },
+    {
+      time: "8 pm",
+      activity: "",
+      button: "Button",
+    },
+  ];
+
+  /* ---------- Event listeners ---------- */
+  init();
+  $(".save").on("click", saveActivity);
+
+  /* ---------- Function declarations ---------- */
+  function init() {
+    displayActivities();
+  }
+
+  function displayActivities() {
+    activitiesArray.forEach(function (activity, index) {
+      // Load activitiesArray from localStorage
+      var activityLoad = "activityWrap" + index;
+      var activityWrapLoaded = localStorage.getItem(`${activityLoad}`);
+      activityWrapLoaded = JSON.parse(activityWrapLoaded);
+      if (localStorage.getItem(`${activityLoad}`) != null) {
+        activitiesArray[index] = activityWrapLoaded;
+      }
+
+      // Create the activity-wraps and display info from activitiesArray
+      var divActWrap = `<div class="activity-wrap input-group">
+                    <div class="input-group-prepend">
+                      <span class="time input-group-text">${activitiesArray[index].time}</span>
+                    </div>
+                    <input type="text" class="activity form-control" placeholder="" value="${activitiesArray[index].activity}">
+                    <div class="input-group-append">
+                      <button class="save btn btn-info" type="button" data-index="${index}">${activitiesArray[index].button}</button>
+                    </div>
+                  </div>`;
+      $("#activities").append(divActWrap);
+    });
+  }
+
+  function saveActivity() {
+    // Save to localStorage
+    var index = $(this).attr("data-index");
+    var time = $("#activities")
+      .children()
+      .eq(index)
+      .children()
+      .eq(0)
+      .children()
+      .eq(0)
+      .text();
+    var activity = $("#activities").children().eq(index).children().eq(1).val();
+    var button = $(this).text();
+    var activityWrap = {
+      time: time,
+      activity: activity,
+      button: button,
+    };
+    var activityWrapText = JSON.stringify(activityWrap);
+    var activitySafe = "activityWrap" + index;
+    localStorage.setItem(activitySafe, activityWrapText);
+
+    // Update activitiesArray
+    console.log(activitiesArray);
+    activitiesArray[index] = activityWrap;
+    console.log(activitiesArray);
+  }
+});
